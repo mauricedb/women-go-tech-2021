18c6acdacd102c3efc36dab43cd6da1a314da783
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-06-14 09:47:44 +0200

    Edit movie props in form

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,24 +1,49 @@
+import { useState } from "react";
+
 export function Movie({ movie }) {
+  const [thisMovie, setThisMovie] = useState(movie);
+
   return (
     <form>
       <div>
         <label>Title</label>
-        <p>{movie.title} </p>
+        <input
+          value={thisMovie.title}
+          onChange={(e) =>
+            setThisMovie({ ...thisMovie, title: e.target.value })
+          }
+        />
       </div>
 
       <div>
         <label>Overview</label>
-        <p>{movie.overview} </p>
+        <textarea
+          value={thisMovie.overview}
+          onChange={(e) =>
+            setThisMovie({ ...thisMovie, overview: e.target.value })
+          }
+          rows={5}
+        />
       </div>
 
       <div>
         <label>Vote average</label>
-        <p>{movie.vote_average} </p>
+        <input
+          value={thisMovie.vote_average}
+          onChange={(e) =>
+            setThisMovie({ ...thisMovie, vote_average: e.target.value })
+          }
+        />
       </div>
 
       <div>
         <label>Release date</label>
-        <p>{movie.release_date} </p>
+        <input
+          value={thisMovie.release_date}
+          onChange={(e) =>
+            setThisMovie({ ...thisMovie, release_date: e.target.value })
+          }
+        />
       </div>
     </form>
   );
