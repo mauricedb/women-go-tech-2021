7282ebb04eef6219b38c61cf500ee487cdd0163d
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-06-14 09:39:40 +0200

    Pass movie object as prop

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import "./App.css";
 
 import { Movie } from "./components/Movie";
 
+const movie19404 = {
+  id: 19404,
+  overview:
+    "Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga.",
+  release_date: "1995-10-20",
+  title: "Dilwale Dulhania Le Jayenge",
+  vote_average: 8.7,
+};
+
 function App() {
   return (
     <div className="App">
-      <Movie />
+      <Movie movie={movie19404} />
     </div>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,33 +1,24 @@
-const movie19404 = {
-  id: 19404,
-  overview:
-    "Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga.",
-  release_date: "1995-10-20",
-  title: "Dilwale Dulhania Le Jayenge",
-  vote_average: 8.7,
-};
-
-export function Movie() {
+export function Movie({ movie }) {
   return (
     <form>
       <div>
         <label>Title</label>
-        <p>{movie19404.title} </p>
+        <p>{movie.title} </p>
       </div>
 
       <div>
         <label>Overview</label>
-        <p>{movie19404.overview} </p>
+        <p>{movie.overview} </p>
       </div>
 
       <div>
         <label>Vote average</label>
-        <p>{movie19404.vote_average} </p>
+        <p>{movie.vote_average} </p>
       </div>
 
       <div>
         <label>Release date</label>
-        <p>{movie19404.release_date} </p>
+        <p>{movie.release_date} </p>
       </div>
     </form>
   );
